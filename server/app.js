ddaed85523534cdc811245b7fabd3ebf70c97d37
Marc1324 <cyemarc00@gmail.com>
2026-03-12 18:36:13 +0100

    actualizada

--- server/app.js
+++ server/app.js
@@ -1,18 +1,24 @@
+/* gestionar servidor y rutas*/
 const express = require('express');
 const hbs = require('hbs');
+/*guestionar rutas en sistemas operativos*/
 const path = require('path');
+/* leer archivos json*/
 const fs = require('fs');
-const { title } = require('process');
-
+/*encender*/
 const app = express();
 
 // Configuració Handlebars
+/*usuar hbs motor vista*/
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+/*registrar carpeta partials para acceder header*/
 hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
+/*ir a la carpeta public para utilizar estils*/
 app.use(express.static(path.join(__dirname, '../public')));
 
 // D) Helper obligatori: lte (less than or equal)
+/*registrar el helper y definimos la funcion por que no viene definido y no prodira leerlo algunas cosas*/
 hbs.registerHelper('lte', function (a, b) {
     return a <= b;
 });
@@ -23,6 +29,7 @@ const cities = JSON.parse(fs.readFileSync(path.join(__dirname, 'data/cities.json
 const countries = JSON.parse(fs.readFileSync(path.join(__dirname, 'data/countries.json'), 'utf8'));
 
 // B) Ruta /
+/*puerto de entrada/ruta principal*/
 app.get('/', (req, res) => {
     res.render('index', site);
 });
